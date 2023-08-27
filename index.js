8f649f81ba63f33edb2b19f1109460b1c11e8c2a
TekleJankhoteli <jankhotelitekla@gmail.com>
2023-08-27 19:27:17 +0400

    firts commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const quoteText=document.querySelector(".quote");
+let authorName=document.querySelector(".author");
+let quoteBtn=document.querySelector("button");
+let soundBtn=document.querySelector(".sound");
+let copyBtn=document.querySelector(".copy");
+
+
+function randomeQuote(){
+
+
+ quoteBtn.innerText="Loading Quote...";
+    fetch("http://api.quotable.io/random").then(res =>res.json()).then(result=>{
+       
+
+        quoteText.innerText=result.content;
+        authorName.innerText=result.author;
+        quoteBtn.innerText="New Quote";
+    
+
+    })
+}
+
+soundBtn.addEventListener("click", ()=>{
+    let utterance=new SpeechSynthesisUtterance(`${quoteText.innerText} by ${ authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+})
+
+
+quoteBtn.addEventListener("click", randomeQuote);
